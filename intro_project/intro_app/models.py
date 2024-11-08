6e99541562f868303ef343a89a0b553356be118f
AudreyKrishnadasan <98282129+AudreyKrishnadasan@users.noreply.github.com>
2024-11-07 17:29:10 -0800

    feat: added correct identifiers to User table and added Post/Comment table

--- intro_project/intro_app/models.py
+++ intro_project/intro_app/models.py
@@ -1,7 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    contents = models.TextField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    contents = models.TextField()
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(default=timezone.now)
+
+
 class User(models.Model):
-  username = models.CharField(max_length=255)
-  isAdmin = models.BooleanField()
+	user_id = models.IntegerField(primary_key=True)
+	email = models.EmailField(unique=True, max_length=255, null=True, blank=True)
+	name = models.CharField(max_length=255, null=True, blank=True)
+	admin = models.BooleanField(default=False)
