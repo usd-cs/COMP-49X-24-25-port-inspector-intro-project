2ed75006e7127b8820cbdd1b3400e986409380c6
Joe Boxberger <jboxberger@sandiego.edu>
2024-11-10 22:08:10 -0800

    Added tests for post object

--- intro_project/intro_app/models.py
+++ intro_project/intro_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Post(models.Model):
     body = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.body
--- intro_project/intro_app/tests.py
+++ intro_project/intro_app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+
+class PostModelTest(TestCase):
+    test_string = "Hello world, test post"
+
+    def setUp(self):
+        # Set up a post object for testing
+        self.post = Post.objects.create(body=self.test_string)
+
+    def test_post_creation(self):
+        # Check that the post is created correctly
+        self.assertEqual(self.post.body, self.test_string)
+        self.assertTrue(self.post.create_date)  # Check if date field is filled
