c7675685150743ebbabccdf2cee790ed7ce597a3
Joe Boxberger <jboxberger@sandiego.edu>
2024-11-12 01:06:55 -0800

    Added basic login functionality for post author

--- intro_project/intro_app/views.py
+++ intro_project/intro_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 from . import forms
 
 from .models import Post
@@ -15,7 +17,7 @@ def view_posts(request):
     return render(request, 'home.html', {'posts': posts})
 
 # This tag might be useful to limit creating a post for only users who are logged in
-#@login_required(login_url="/users/login")
+#@login_required(login_url="/login")
 def new_post(request):
     # if the user is attempting to POST
     if request.method == "POST":
@@ -23,6 +25,10 @@ def new_post(request):
         if form.is_valid():
             # if body is valid, save it
             newpost = form.save(commit=False)
+            #save the author based on login of the request
+            print("TEST")
+            print(type(request.user))
+            newpost.user = request.user
             newpost.save()
             #redirect the user back to homepage after post
             return redirect("/")
@@ -31,3 +37,18 @@ def new_post(request):
         form = forms.CreatePost()
     
     return render(request, 'createpost.html', { 'form' : form }) #passes in the form object we create above to our template, for rendering
+
+
+def login_view(request):
+    # if recieveing a POST method, user is attempting to login
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        # if user is properly authenticated
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("/") # after the user logs in, send them to the homepage
+    # if requesting the page, prompt form for authentication
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', { 'form' : form })
